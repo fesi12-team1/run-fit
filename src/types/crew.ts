0cfc183d7cd97804e37d9750e0a73f5de5d7d073
Junyeong Heo <hhjjyy1122@gmail.com>
2025-12-01 17:38:49 +0900

    chore: Backend API에 맞춰 DTO 추가 수정

--- src/types/crew.ts
+++ src/types/crew.ts
@@ -1,13 +1,10 @@
-import { Session } from './session';
-import { Member } from './user';
-
 export interface Crew {
   id: number;
   name: string;
   description: string;
   city: string;
   image: string;
-  members: Member[];
-  sessions: Session[];
   createdAt: string;
+  // members: Member[];
+  // sessions: Session[];
 }
--- src/types/session.ts
+++ src/types/session.ts
@@ -1,22 +1,25 @@
 export interface Session {
   id: string;
   crewId: string;
-  userId: string;
-  status: 'OPEN' | 'CLOSED';
+  userId: string; // host user id
 
   name: string;
   description: string;
   image?: string;
-  city: string;
-  district: string;
+
+  location: string;
+  // TODO: 이 부분은 지도 관련 결정이 나면 수정 필요
+  // city: string;
+  // district: string;
+
   createdAt: string;
   sessionAt: string;
   registerBy: string;
   level: 'beginner' | 'intermediate' | 'advanced';
 
-  maxParticipantCount: number;
+  status: 'OPEN' | 'CLOSED';
+  pace: number; // 분/km
 
-  // participants: Member[];
-  // likedUsers: User[];
-  // reviews: Review[];
+  maxParticipantCount: number;
+  currentParticipantCount: number;
 }
