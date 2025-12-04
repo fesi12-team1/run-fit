4ab81102280f2cba9c93aa5650eeab5390f2a185
seoleunjin <anah9026@gmail.com>
2025-12-04 13:45:34 +0900

    chore(design): 공통 디자인 시스템 및 초기 스타일 세팅
    - 기본 색상, 폰트 토큰 정의
    - base layer CSS 초기화 및 html/body 스타일 설정
    - Tailwind, 폰트, theme, utilities 연동
    - display, body, caption 텍스트 유틸리티 정의

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center">
-      Hello, world!
+    <div className="flex h-screen items-center justify-center">
+      <p className="text-display-lg-semibold bg-(--color-primary) p-5">
+        Hello, world!
+      </p>
     </div>
   );
 }
