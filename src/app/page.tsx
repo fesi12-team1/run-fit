5f5a577eb2b617ef0c5b42f2953276dd1308f3ef
seoleunjin <anah9026@gmail.com>
2025-12-04 14:41:33 +0900

    chore(design): 공통 디자인 시스템 및 초기 스타일 세팅 일부 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <p className="text-display-lg-semibold bg-(--color-primary) p-5">
-        Hello, world!
-      </p>
+      <p className="text-display-lg-semibold bg-brand-700 p-5">Hello, world!</p>
     </div>
   );
 }
