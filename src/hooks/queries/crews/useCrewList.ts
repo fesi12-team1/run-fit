e7d52c9a3c6d98a4b7401b4699779ea08387f3af
Yun-Jinwoo <yjw3480@naver.com>
2025-12-10 11:34:43 +0900

    feat: 크루 목록 조회 Hook useCrewList 구현

---
+++ src/hooks/queries/crews/useCrewList.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import { getCrews } from '@/api/crews';
+import { QUERY_KEYS } from '@/lib/constants';
+import { CrewListFilters } from '@/types';
+
+/**
+ * 크루 목록 조회 (필터링)
+ * - 조건(필터)에 맞는 러닝 크루 목록을 조회합니다.
+ * @param filters - 지역, 날짜, 시간, 난이도 필터 옵션
+ */
+export default function useCrewList(filters: CrewListFilters) {
+  return useQuery({
+    queryKey: QUERY_KEYS.crews.list(filters),
+    queryFn: () => getCrews(filters), // 필터가 변경되어 데이터를 새로 불러올 때 화면이 깜빡이는 현상 방지
+    placeholderData: (previousData) => previousData, // 1분동안 fresh 상태
+    staleTime: 1000 * 60,
+  });
+}
