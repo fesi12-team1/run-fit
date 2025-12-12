3820ed4b9ce4b002f3352da5a1c823a15a8b00aa
Yun-Jinwoo <yjw3480@naver.com>
2025-12-12 18:12:44 +0900

    feat: session API 함수 관련 Query Factory 구현

---
+++ src/api/queries/sessionQueries.ts
@@ -0,0 +1,41 @@
+import { queryOptions } from '@tanstack/react-query';
+import {
+  getSessionDetail,
+  getSessionParticipants,
+  getSessions,
+} from '@/api/fetch/sessions';
+import { normalizeParams } from '@/lib/utils';
+import { SessionListFilters } from '@/types';
+
+export const sessionQueries = {
+  all: () => ['sessions'],
+
+  // 세션 목록 조회
+  lists: () => [...sessionQueries.all(), 'list'],
+  list: (filters: SessionListFilters) => {
+    const cleanFiters = normalizeParams(filters);
+    return queryOptions({
+      queryKey: [...sessionQueries.lists(), cleanFilters],
+      queryFn: () => getSessions(cleanFiters),
+      placeholderData: (previousData) => previousData, // 필터가 변경되어 데이터를 새로 불러올 때 화면이 깜빡이는 현상 방지
+      staleTime: 1000 * 60, // 1분동안 fresh 상태
+    });
+  },
+
+  // 세션 상세 정보 조회
+  details: () => [...sessionQueries.all(), 'detail'],
+  detail: (sessionId: number) =>
+    queryOptions({
+      queryKey: [...sessionQueries.details(), sessionId],
+      queryFn: () => getSessionDetail(sessionId),
+      enabled: !!sessionId, // sessionId가 유효할 때만 실행
+    }),
+
+  // 세션 참가자 조회
+  participants: (sessionId: number) =>
+    queryOptions({
+      queryKey: [...sessionQueries.detail(sessionId).queryKey, 'participants'],
+      queryFn: () => getSessionParticipants(sessionId),
+      enabled: !!sessionId, // sessionId가 유효할 때만 실행
+    }),
+};
