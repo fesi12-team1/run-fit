312caa1ad1a5a712ba296c43b5077b08cea41f5c
Shion <seoy1108@gmail.com>
2025-12-16 01:38:15 +0900

    feat: 헤더 컴포넌트에서 로고 SVG 파일을 이미지로 변경

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import Hamburger from '@/assets/icons/hamburger.svg';
-import Logo from '@/assets/layout/logo.svg';
 
 export default function Header() {
   const { isLoggedIn, user } = { isLoggedIn: true, user: { image: null } };
@@ -11,7 +11,20 @@ export default function Header() {
     <header className="tablet:h-15 tablet:px-6 sticky top-0 z-50 h-14 w-full border-b border-b-gray-600 bg-gray-800 px-4">
       <div className="tablet:gap-5 mx-auto flex h-full max-w-[1198px] items-center justify-between gap-3">
         <Link href="/">
-          <Logo />
+          <Image
+            src="/assets/symbol.png"
+            alt="Symbol"
+            width={40}
+            height={40}
+            className="tablet:hidden block"
+          />
+          <Image
+            src="/assets/symbol-text.png"
+            alt="Symbol"
+            width={120}
+            height={40}
+            className="tablet:block hidden"
+          />
         </Link>
         <nav className="text-body3-semibold tablet:text-body2-semibold tablet:gap-6 flex h-full flex-1 items-stretch gap-3">
           <Link href="/sessions" className="flex items-center">
