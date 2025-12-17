564529bba4f40fc7e308c5d76e48457183404f1f
Shion <seoy1108@gmail.com>
2025-12-17 16:22:00 +0900

    refactor: Input 컴포넌트에서 에러 메시지 처리 개선 및 로그인 스키마 유효성 검사 수정

--- src/lib/validations/auth/signinSchema.ts
+++ src/lib/validations/auth/signinSchema.ts
@@ -6,7 +6,10 @@ export const signinSchema = z.object({
     .min(1, '이메일을 입력해주세요.')
     .email('올바른 이메일 형식이 아닙니다.'),
 
-  password: z.string().min(1, '비밀번호를 입력해주세요.'),
+  password: z
+    .string()
+    .min(2, '비밀번호를 입력해주세요.')
+    .min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
 });
 
 export type SigninFormValues = z.infer<typeof signinSchema>;
