d82fd174a3a94763ceb10f08bab91f7f231dbcdb
Yun-Jinwoo <yjw3480@naver.com>
2025-12-18 17:48:29 +0900

    feat: mobile, tablet만 접근 가능한 my 홈 페이지 구현

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <div>page</div>;
+'use client';
+
+import { redirect } from 'next/navigation';
+import MyInfo from '@/components/my/MyInfo';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+
+export default function MyPage() {
+  const isLaptop = useMediaQuery('laptop');
+  const isDesktop = useMediaQuery('desktop');
+  const isPc = isLaptop || isDesktop;
+
+  if (isPc) {
+    redirect('/my/sessions');
+  }
+
+  return <MyInfo />;
 }
