949e1e1c20f49ff55e73041ae099f5db798da6ae
Yun-Jinwoo <yjw3480@naver.com>
2025-12-28 21:53:45 +0900

    feat: 리뷰 카드 컴포넌트 이미지 추가 및 수정

--- src/components/crew/ReviewCard/index.tsx
+++ src/components/crew/ReviewCard/index.tsx
@@ -4,14 +4,19 @@ import { useQuery } from '@tanstack/react-query';
 import { sessionQueries } from '@/api/queries/sessionQueries';
 import SessionInfo from '@/components/session/SessionInfo';
 import Rating from '@/components/ui/Rating';
+import SafeImage from '@/components/ui/SafeImage';
 import UserAvatar from '@/components/ui/UserAvatar';
 import { Review } from '@/types';
 
 interface ReviewCardProps {
   data: Review;
+  showUser?: boolean;
 }
 
-export default function ReviewCard({ data: review }: ReviewCardProps) {
+export default function ReviewCard({
+  data: review,
+  showUser = true,
+}: ReviewCardProps) {
   const { data: userSession } = useQuery(
     sessionQueries.detail(review.sessionId)
   );
@@ -19,12 +24,30 @@ export default function ReviewCard({ data: review }: ReviewCardProps) {
   const createdAtText = `${createdAt.getFullYear()}.${createdAt.getMonth() + 1}.${createdAt.getDate()}`;
   return (
     <li className="flex flex-col gap-2">
+      {review.image && (
+        <SafeImage
+          src={review.image}
+          fallbackSrc="/assets/profile-default.png"
+          alt="리뷰 이미지"
+          width={124}
+          height={80}
+          className="rounded-xl"
+        />
+      )}
       <Rating value={review.ranks} disabled onChange={() => {}} />
       <div className="text-body3-regular">{review.description}</div>
-      <div className="*:text-caption-regular flex items-center gap-2 *:text-gray-300">
-        <UserAvatar src={review.userImage} alt={review.userName} />
-        <div>{review.userName}</div>
-        <div>|</div>
+      <div className="text-caption-regular flex items-center gap-2 text-gray-300">
+        {showUser && (
+          <>
+            <UserAvatar
+              src={review.userImage}
+              alt={review.userName}
+              className="size-6"
+            />
+            <div>{review.userName}</div>
+            <div>|</div>
+          </>
+        )}
         <div>{createdAtText}</div>
       </div>
       {userSession && <SessionInfo data={userSession} />}
