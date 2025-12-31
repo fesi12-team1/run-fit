23bb62b865f1fca13d79c6d2300163af52108afe
Yun-Jinwoo <yjw3480@naver.com>
2025-12-31 10:40:37 +0900

    refactor: useSearchParams를 위해 suspense 적용

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,30 +1,17 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
-import { toast } from 'sonner';
-import SigninForm from '@/components/auth/SigninForm';
-import AuthLayout from '@/components/layout/AuthLayout';
+import { Suspense } from 'react';
+import Spinner from '@/components/ui/Spinner';
+import SigninClient from './SigninClient';
 
 export default function SigninPage() {
-  const searchParams = useSearchParams();
-  const reason = searchParams.get('reason');
-
-  useEffect(() => {
-    if (reason === 'auth') {
-      toast.info('로그인이 필요한 페이지예요', {
-        id: 'auth-required',
-      });
-    }
-  }, [reason]);
-
   return (
-    <AuthLayout
-      title="로그인"
-      form={<SigninForm />}
-      footerText="계정이 없으신가요?"
-      footerLinkText="회원가입"
-      footerLinkHref="/signup"
-    />
+    <Suspense
+      fallback={
+        <div className="h-main flex items-center justify-center">
+          <Spinner className="text-brand-500 size-8" />
+        </div>
+      }
+    >
+      <SigninClient />
+    </Suspense>
   );
 }
