da29c12926e689715b625ddca6c084f80a1e163a
Seokjun.Kang <kangsj0327@gmail.com>
2026-01-02 15:23:40 +0900

    fix: h-main 유틸리티 클래스에 고정 높이 삭제 후 세션 상세페이지에만 적용

--- src/app/sessions/[id]/page.tsx
+++ src/app/sessions/[id]/page.tsx
@@ -10,7 +10,7 @@ export default async function Page({
 
   return (
     <>
-      <main className="h-main laptop:bg-gray-900 bg-gray-800">
+      <main className="h-main laptop:bg-gray-900 tablet:h-[calc(100vh-60px)] h-[calc(100vh-56px)] bg-gray-800">
         <SessionDetailContainer sessionId={sessionId} />
       </main>
       <BottomBar sessionId={sessionId} />
