e3555c49e0add65fa7ce64292696bae8806fdcc7
Seokjun.Kang <kangsj0327@gmail.com>
2026-01-14 18:28:46 +0900

    chore: stackReducer.ts에 GET_TOP 타입 삭제

--- src/provider/ModalProvider/index.tsx
+++ src/provider/ModalProvider/index.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-// Re-exports
 export { useModalContext } from './IndividualModalContext';
 export { ModalControllerProvider } from './ModalController';
 export { useModalController } from './ModalController';
